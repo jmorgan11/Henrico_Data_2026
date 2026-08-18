975c39b7fb17c53663d1137f3b27c7497cb1fbe2
Jesse Morgan <jmorgan1371@gmail.com>
2026-08-18 06:51:13 -0400

    Clean up community_increase.py code.

--- src/community_increase.py
+++ src/community_increase.py
@@ -2,12 +2,14 @@
 Script Name: new_community_increase.py
 Author: Jesse Morgan
 Date: 2024-06-19
-Description: This script contains queries related to datasets where the community flooding increased
+Description: This script contains queries related to datasets 
+             where the community flooding increased
 """
+import sys
 from collections import OrderedDict
 import arcpy
 from config import BUILDINGS, TAX_PARCELS
-import sys
+
 
 QUERY_DICT = OrderedDict([
     ("Parcels with increasing Community Floodplains areas", 0),
@@ -25,20 +27,36 @@ def main(parcels, buildings):
     """
     try:
         # Parcels with increasing community flooding
-        selected = arcpy.SelectLayerByAttribute_management(parcels, "NEW_SELECTION", "com_increase = 'T'")
-        QUERY_DICT["Parcels with increasing Community Floodplains areas"] = int(arcpy.GetCount_management(selected).getOutput(0))
+        selected = arcpy.SelectLayerByAttribute_management(
+            in_layer_or_view=parcels,
+            selection_type="NEW_SELECTION",
+            where_clause="com_increase = 'T'")
+
+        QUERY_DICT["Parcels with increasing Community Floodplains areas"] = \
+            int(arcpy.GetCount_management(in_rows=selected).getOutput(0))
 
         # Buildings with increasing community flooding
-        selected = arcpy.SelectLayerByAttribute_management(buildings, "NEW_SELECTION", "com_increase = 'T'")
-        QUERY_DICT["Buildings with increasing Community Floodplains areas"] = int(arcpy.GetCount_management(selected).getOutput(0))
+        selected = arcpy.SelectLayerByAttribute_management(
+            in_layer_or_view=buildings,
+            selection_type="NEW_SELECTION",
+            where_clause="com_increase = 'T'")
+
+        QUERY_DICT["Buildings with increasing Community Floodplains areas"] = \
+            int(arcpy.GetCount_management(in_rows=selected).getOutput(0))
 
         # Buildings with increasing community flooding and > 600 square feet
-        selected = arcpy.SelectLayerByAttribute_management(buildings, "NEW_SELECTION", "com_increase = 'T' And sq_ft >= 600")
-        QUERY_DICT["Buildings with increasing Community Floodplains areas > 600 square feet"] = int(arcpy.GetCount_management(selected).getOutput(0))
+        selected = arcpy.SelectLayerByAttribute_management(
+            in_layer_or_view=buildings,
+            selection_type="NEW_SELECTION",
+            where_clause="com_increase = 'T' And sq_ft >= 600")
+
+        QUERY_DICT["Buildings with increasing Community " \
+        "Floodplains areas > 600 square feet"] = \
+            int(arcpy.GetCount_management(in_rows=selected).getOutput(0))
 
     except arcpy.ExecuteError:
         print(arcpy.GetMessages())
-        sys.exit(1)        
+        sys.exit(1)
 
     return QUERY_DICT
 
@@ -46,4 +64,4 @@ if __name__ == '__main__':
     result = main(parcels=TAX_PARCELS, buildings=BUILDINGS)
 
     for key, value in result.items():
-        print(f"{key}: {value}")     
+        print(f"{key}: {value}")
