3dbda88163a2de110771d2f6a77f026d82c0fd2b
jmorgan11 <jmorgan1371@gmail.com>
2026-08-19 08:28:11 -0400

    Create community_no_change.py script.

--- src/community_no_change.py
+++ src/community_no_change.py
@@ -1,24 +1,20 @@
 """
-Script Name: community_no_change.py
+Script Name: new_community_increase.py
 Author: Jesse Morgan
 Date: 2024-06-19
-Description: This script contains queries related to datasets
-             where there is no community floodplain change.
+Description: This script contains queries related to datasets 
+             where the community flooding no change.
 """
 import sys
 from collections import OrderedDict
 import arcpy
 from config import BUILDINGS, TAX_PARCELS
 
+
 QUERY_DICT = OrderedDict([
-    ("Parcels who's only new condition is no change "
-    "to the Community Floodplain (No Change Only)", 0),
-
-    ("Buildings who's only new condition is no change to "
-    "the Community Floodplain (No Change Only)", 0),
-
-    ("Buildings who's only new condition is no change to the "
-    "Community Floodplain (No Change Only) > 600 square feet", 0)
+    ("Parcels within Community Floodplains but no change is occurring", 0),
+    ("Buildings within Community Floodplains but no change is occurring", 0),
+    ("Buildings within Community Floodplains but no change is occurring > 600 square feet", 0)
 ])
 
 def main(parcels, buildings, magistrate=None):
@@ -31,42 +27,37 @@ def main(parcels, buildings, magistrate=None):
     magistrate (str: Optional): The magistrate name to query on.
     """
     try:
-        where_clause = "com_no_change = 'T' And " \
-            "com_decrease = 'F' And " \
-            "com_increase = 'F' And " \
-            "community_flooding = 'T'"
+        where_clause = "com_no_change = 'T'"
 
         if magistrate:
             where_clause += f" And magistrate = '{magistrate}'"
 
-        # Parcels with increasing community flooding
+        # Parcels with no change in community flooding
         selected = arcpy.SelectLayerByAttribute_management(
             in_layer_or_view=parcels,
             selection_type="NEW_SELECTION",
             where_clause=where_clause)
 
-        QUERY_DICT["Parcels who's only new condition is no change " \
-        "to the Community Floodplain (No Change Only)"] = \
+        QUERY_DICT["Parcels within Community Floodplains but no change is occurring"] = \
             int(arcpy.GetCount_management(in_rows=selected).getOutput(0))
 
-        # Buildings with increasing community flooding
+        # Buildings with no change in community flooding
         selected = arcpy.SelectLayerByAttribute_management(
             in_layer_or_view=buildings,
             selection_type="NEW_SELECTION",
             where_clause=where_clause)
 
-        QUERY_DICT["Buildings who's only new condition is no change " \
-        "to the Community Floodplain (No Change Only)"] = \
+        QUERY_DICT["Buildings within Community Floodplains but no change is occurring"] = \
             int(arcpy.GetCount_management(in_rows=selected).getOutput(0))
 
-        # Buildings with increasing community flooding and > 600 square feet
+        # Buildings with no change community flooding and > 600 square feet
         selected = arcpy.SelectLayerByAttribute_management(
             in_layer_or_view=buildings,
             selection_type="NEW_SELECTION",
-            where_clause=where_clause +" And sq_ft >= 600")
+            where_clause=where_clause + " And sq_ft >= 600")
 
-        QUERY_DICT["Buildings who's only new condition is no change to the " \
-        "Community Floodplain (No Change Only) > 600 square feet"] = \
+        QUERY_DICT["Buildings within Community Floodplains but no change "
+        "is occurring > 600 square feet"] = \
             int(arcpy.GetCount_management(in_rows=selected).getOutput(0))
 
     except arcpy.ExecuteError:
